c43494bae5595380ddb8ff7a6c6d3317a6cfd00b
Niraj chaudhary <np03cs4a220219@heraldcollege.edu.np>
2022-12-06 09:36:37 +0545

    first commit

---
+++ hello/heyy.java
@@ -0,0 +1,10 @@
+public class heyy{
+    public static void main(String args[]){
+        int mass = 12;
+        double gravity = 9.8;
+        int  height = 5;
+        double PE = (mass*gravity*height);
+        System.out.println("the potential energy of a body"+PE);
+        
+    }
+}
